df3bbbfc8f96af3d6be14bf6d229b9490cb72395
Kyle Andrews <kylea@page.works>
2019-11-29 16:44:41 -0500

    Adds carousel slide switching

--- templates/homepage/_hero-carousel/hero-carousel.ts
+++ templates/homepage/_hero-carousel/hero-carousel.ts
@@ -1,11 +1,30 @@
 class HeroCarouselComponent extends HTMLElement {
 	private slides: Array<HTMLElement>;
 	private buttons: Array<HTMLButtonElement>;
+	private index: number;
 
 	constructor() {
 		super();
 		this.slides = Array.from(this.querySelectorAll('carousel-slide'));
 		this.buttons = Array.from(this.querySelectorAll('carousel-controls button'));
+		this.index = 0;
+	}
+
+	private switchSlide(newSlideIndex: number): void {
+		const currentSlide = this.slides[this.index];
+		const newSlide = this.slides[newSlideIndex];
+
+		currentSlide.style.opacity = '0';
+		currentSlide.style.visibility = 'hidden';
+		currentSlide.style.zIndex = '1';
+		currentSlide.classList.remove('is-active');
+
+		newSlide.style.opacity = '1';
+		newSlide.style.visibility = 'visible';
+		newSlide.style.zIndex = '5';
+		newSlide.classList.add('is-active');
+
+		this.index = newSlideIndex;
 	}
 
 	private handleButtonClickEvent = (e: Event) => {
@@ -14,6 +33,7 @@ class HeroCarouselComponent extends HTMLElement {
 			button.classList.remove('is-active');
 		});
 		button.classList.add('is-active');
+		this.switchSlide(parseInt(button.dataset.index));
 	};
 
 	connectedCallback() {
