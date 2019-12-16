1d75e4ffd6020bdf6a9089d8146f239ba94f359c
Kyle Andrews <kylea@page.works>
2019-12-16 06:43:02 -0500

    Adds ability to micromanage section spacing

--- config/matrixmate.php
+++ config/matrixmate.php
@@ -18,6 +18,221 @@ return [
                         'types' => ['ctaGrid', 'ctaCards', 'ctaBlogCards', 'ctaBanner'],
                     ]
                 ],
+                'types' => [
+                    'ctaGrid' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy', 'button'],
+                            ],
+                            [
+                                'label' => 'Cards',
+                                'fields' => ['cards'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'ctaCards' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy', 'button'],
+                            ],
+                            [
+                                'label' => 'Cards',
+                                'fields' => ['cards'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'pullQuote' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['quote', 'author', 'authorsTitle'],
+                            ],
+                            [
+                                'label' => 'Assets',
+                                'fields' => ['image'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'ctaBlogCards' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy', 'button'],
+                            ],
+                            [
+                                'label' => 'Cards',
+                                'fields' => ['cards'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'ctaBanner' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy', 'button'],
+                            ],
+                            [
+                                'label' => 'Assets',
+                                'fields' => ['backgroundImage'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'storyBlock' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy', 'customLink'],
+                            ],
+                            [
+                                'label' => 'Assets',
+                                'fields' => ['image'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'descriptiveList' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading'],
+                            ],
+                            [
+                                'label' => 'List',
+                                'fields' => ['lists'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'statisticsShowcase' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading'],
+                            ],
+                            [
+                                'label' => 'Cards',
+                                'fields' => ['cards'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'teamShowcase' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Team',
+                                'fields' => ['teamMembers'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'richText' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'headingPosition', 'copy'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'image' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Assets',
+                                'fields' => ['image', 'cite'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'video' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Video Settings',
+                                'fields' => ['videoProvider', 'videoId', 'thumbnailImage'],
+                            ],
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'location' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Details',
+                                'fields' => ['companyName', 'location', 'hoursOfOperation', 'directionLink', 'contactEmail'],
+                            ],
+                            [
+                                'label' => 'Map',
+                                'fields' => ['map', 'mapImage'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                ],
             ],
             'section:blog' => [
                 'groups' => [
@@ -29,6 +244,102 @@ return [
                         'label' => 'Calls to Action',
                         'types' => ['ctaBlogCards', 'ctaBanner'],
                     ]
+                ],
+                'types' => [
+                    'richText' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'headingPosition', 'copy'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'image' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Assets',
+                                'fields' => ['image', 'cite'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'video' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Video Settings',
+                                'fields' => ['videoProvider', 'videoId', 'thumbnailImage'],
+                            ],
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'pullQuote' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['quote', 'author', 'authorsTitle'],
+                            ],
+                            [
+                                'label' => 'Assets',
+                                'fields' => ['image'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'ctaBlogCards' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy', 'button'],
+                            ],
+                            [
+                                'label' => 'Cards',
+                                'fields' => ['cards'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'ctaBanner' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy', 'button'],
+                            ],
+                            [
+                                'label' => 'Assets',
+                                'fields' => ['backgroundImage'],
+                            ],
+                            [
+                                'label' => 'Spacing',
+                                'fields' => ['spacing'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
                 ],
                 'hideUngroupedTypes' => true,
             ]
