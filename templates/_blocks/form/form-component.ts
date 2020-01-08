e8d358e751e3b9d42a94532c3c28fa0646108ad2
Kyle Andrews <kylea@page.works>
2020-01-08 07:26:24 -0500

    Added page break functionality

--- templates/_blocks/form/form-component.ts
+++ templates/_blocks/form/form-component.ts
@@ -1,23 +1,58 @@
 class FormComponent extends HTMLElement {
 	private form: HTMLFormElement;
+	private nextButtons: Array<HTMLButtonElement>;
+	private pages: Array<HTMLElement>;
+	private currPageIndex: number;
 	constructor() {
 		super();
+		this.currPageIndex = 0;
 		this.form = this.querySelector('form');
+		this.nextButtons = Array.from(this.form.querySelectorAll('button[type="button"]'));
+		this.pages = Array.from(this.form.querySelectorAll('form-page'));
+	}
+
+	private validatePage(page: HTMLElement): boolean {
+		let valid = true;
+		page.querySelectorAll('input, select, textarea').forEach((input: HTMLInputElement) => {
+			if (!input.checkValidity() && valid) {
+				valid = false;
+				input.reportValidity();
+			}
+		});
+		return valid;
 	}
 
 	private submitForm(e: Event): void {
 		e.preventDefault();
 		const data = new FormData(this.form);
-		console.log(data);
+		if (this.validatePage(this.pages[this.currPageIndex])) {
+			console.log('valid');
+		}
 	}
 	private handleFormSubmit: EventListener = this.submitForm.bind(this);
+
+	private handleNext(e: Event): void {
+		const target = e.currentTarget as HTMLButtonElement;
+		const nextPageNum = parseInt(target.dataset.nextPage);
+		if (this.validatePage(this.pages[this.currPageIndex])) {
+			this.pages[this.currPageIndex].style.display = 'none';
+			this.currPageIndex = nextPageNum;
+			this.pages[this.currPageIndex].style.display = 'block';
+
+			if (this.pages.length - 1 === this.currPageIndex) {
+				const subButton = this.form.querySelector('button[type="submit"]') as HTMLButtonElement;
+				subButton.parentElement.style.display = 'flex';
+			}
+		}
+	}
+	private handleNextButtonClick: EventListener = this.handleNext.bind(this);
 
 	connectedCallback() {
 		this.form.addEventListener('submit', this.handleFormSubmit);
 
-		const subButton = this.form.querySelector('button[type="submit"]') as HTMLElement;
-		subButton.style.opacity = '1';
-		subButton.style.visibility = 'visible';
+		this.nextButtons.map(button => {
+			button.addEventListener('click', this.handleNextButtonClick);
+		});
 	}
 }
 customElements.define('form-component', FormComponent);
