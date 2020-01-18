75f1e375a7f261abb3e6e070ed0c20c33ffecc9a
Kyle Andrews <kylea@page.works>
2020-01-18 08:21:58 -0500

    Updated NPM packages

--- cssmonster.config.js
+++ cssmonster.config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	outDir: "_css",
 	sources: ["./templates"],
+	minify: true,
+	purge: true,
 	blacklist: ["./templates/frameworks"],
 	purgeCSS: {
 		content: ["./templates/**/*.twig"],
